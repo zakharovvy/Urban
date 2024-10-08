0e1782d878c40dc9f3fd376f0754a72c1e03ec30
zakharov.vy <zakharov.vy@gmail.com>
2024-10-08 18:56:30 +0300

    Задание на создание матрицы

---
+++ module_2_5.py
@@ -0,0 +1,13 @@
+def get_matrix (n, m, value):
+    matrix = []
+    for i in range (n): #Создание внешнего цикла, который создаст внутри списка matrix пустые списки
+        matrix.append ([])
+        for j in range(m): #Создание цикла который будет передавать во внутрь списков значения опр. кол-во раз
+            matrix [i].append(value)
+    return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
