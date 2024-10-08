ef03cc021025fc2224c68db1a2c6c465056e10af
zakharov.vy <zakharov.vy@gmail.com>
2024-10-08 14:20:23 +0300

    Цикл while

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while i < len(my_list):
+    if my_list [i] > 0:
+        print(my_list[i])
+        i = i + 1
+    if my_list [i] == 0:
+        i = i + 1
+        continue
+    if my_list [i] < 0:
+        break
+#
+
