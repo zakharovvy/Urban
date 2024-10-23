280c774ef7dea79b153812008c9a89aae548c87b
zakharov.vy <zakharov.vy@gmail.com>
2024-10-23 20:42:19 +0300

    Домашняя работа по уроку "Модули и пакеты"

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from true_math import divide as divide_true
+from fake_math import divide as divide_fake
+result1 = divide_fake(11661, 3)
+result2 = divide_fake(117, 0)
+result3 = divide_true(3500, 7)
+result4 = divide_true(87505, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
