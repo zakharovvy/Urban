4b63bb19db0e43823f3fb9d9e22b0c7dc302a6af
zakharov.vy <zakharov.vy@gmail.com>
2024-10-29 12:39:20 +0300

    Домашняя работа по уроку "Пространство имен."

---
+++ module_4_2.py
@@ -0,0 +1,8 @@
+
+def test_function ():
+    def inner_function ():
+        print("Я в области видимости функции test_function")
+    inner_function()
+test_function()
+
+#inner_function () - выводит ошибку NameError: name 'inner_function' is not defined
