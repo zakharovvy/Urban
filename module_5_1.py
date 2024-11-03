a54d003a52ff5542e53d22a88e2bfd007c2fdc70
zakharov.vy <zakharov.vy@gmail.com>
2024-11-03 18:44:08 +0300

    Домашняя работа по уроку "Атрибуты и методы объекта"

--- module_5_1.py
+++ module_5_1.py
@@ -4,7 +4,7 @@ class House:
         self.number_of_floors = number_of_floors
 
     def go_to (self, new_floor:int):
-        if new_floor > self.number_of_floors:
+        if new_floor > self.number_of_floors or new_floor < 1:
             print('Такого этажа не существует')
         else:
             for i in range(1, new_floor+1):
@@ -14,5 +14,6 @@ h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
+h2.go_to(-1)
 print(h1.name,h1.number_of_floors)
 print(h2.name, h2.number_of_floors)
