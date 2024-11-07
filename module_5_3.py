aaf2b1fc8180f90e1eaee6182abee2a42ce62e9a
zakharov.vy <zakharov.vy@gmail.com>
2024-11-07 15:58:14 +0300

    Домашняя работа по уроку "Перегрузка операторов"

--- module_5_3.py
+++ module_5_3.py
@@ -72,9 +72,34 @@ class House:
         else:
             return 'Увеличение числа этажей невозможно. Значение value введено некорректно'
 
+    def __radd__(self, value):
+       return self.__add__ (value)
 
+    def __iadd__(self, value):
+        return self.__add__(value)
 
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2)
+
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+
+h1 += 10
+print(h1)
+
+h2 = 10 + h2
+print(h2)
+
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
 
